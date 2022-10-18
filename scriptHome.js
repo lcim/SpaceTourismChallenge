c150999c4f9368d7c0c5faec389231d83e013c00
Chiadi <lcim1104@gmail.com>
2022-10-18 20:06:21 +0100

    Edited "selected" class default

--- scriptHome.js
+++ scriptHome.js
@@ -68,7 +68,12 @@ checkDeviceWidth.addEventListener("click", modifyMenuItems(checkDeviceWidth)); /
 
 const menuIconPath = window.location.pathname;
 const menuIcons = document.querySelectorAll(".navbar a").forEach(link => {
-  if (link.href.includes(`${menuIconPath}`)) {
+  if (link.href === "") {
+    document.querySelectorAll("#id-navbar a")[0].classList.add("selected")
+    console.log(link);
+  }
+  else if (link.href.includes(`${menuIconPath}`)) {
+    //console.log(link);
     link.classList.add("selected");
     //console.log(link);
   }
